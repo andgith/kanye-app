de8c78dcdc2c208b4562b52ad1cb592414720802
Andrew Hirst <andrew.hirst@visavvi.com>
2024-01-08 12:15:01 +0000

    Scaffold tests

---
+++ tests/Feature/GetQuotesTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class GetQuotesTest extends TestCase
+{
+    /** @test */
+    public function it_can_get_random_quotes()
+    {
+        //
+    }
+
+    /** @test */
+    public function it_authenticates_with_api_key()
+    {
+        //
+    }
+}
